1f4dffd99dafb9e354da0b034a90c9e39301be50
luis3 <luis3@LAPTOP-I5BIBL0E>
2020-04-24 20:57:14 -0500

    Libreiras exteriores

---
+++ src/conversionde_json/Gson.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversionde_json;
+
+/**
+ */
+class Gson {
+
+    String toJson(Datos datos) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Datos fromJson(String json, Class<Datos> aClass) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
